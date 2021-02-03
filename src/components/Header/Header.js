a49272489a8ca3fdf3a4d95ffeb8fc7417975b0f
Fredy <fredy.ly.wcs@gmail.com>
2021-02-03 17:17:40 +0100

    adding footer and header components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,51 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import { 
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown
+ } from 'reactstrap';
+import logo from '../../images/logo.png';
 import './Header.css';
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+  const navstyle = {
+    width:'150px', 
+    margin: '15px'
+  }
+  
   return (
-      <div>Header</div>
+    <Navbar color="light" light expand="md">
+      <NavbarBrand href="/">
+      </NavbarBrand>
+        <img src={logo} alt='logo' style={navstyle}/>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="mr-auto" navbar>
+        <NavItem>
+            <NavLink href="/">Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/royal-family">Royal Family</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/assets">Assets</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/contact">Contact Us</NavLink>
+          </NavItem>
+          <UncontrolledDropdown nav inNavbar>
+          </UncontrolledDropdown>
+        </Nav>
+      </Collapse>
+    </Navbar>
   );
 };
 
